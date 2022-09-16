b721ce9703e9009ba0dcffa895e26c228015e2ee
larissavilelasobral <larissavilelasobral@gmail.com>
2022-09-16 14:04:25 -0300

    test

--- routes.py
+++ routes.py
@@ -3,7 +3,12 @@ from main import ladder
 
 app = Flask("Ladder")
 
-@app.route("/ladder", methods=["GET"])
-def get_ladder():
-    return ladder()
+def __init__(self, ):
+    @app.route("/ladder", methods=["GET"])
+    def get_ladder():
+        return ladder()
+
 app.run()
+
+
+
